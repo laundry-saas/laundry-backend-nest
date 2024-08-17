ce28751b999edf8b319a4e90eb5e1d9fcb1bdd73
Iyke Israel <iykeprince48@gmail.com>
2024-08-17 23:51:55 +0100

    cleanup

--- src/modules/transaction/transaction.controller.ts
+++ src/modules/transaction/transaction.controller.ts
@@ -1,6 +1,4 @@
-import {
-  Controller,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 
 @Controller('transaction')
